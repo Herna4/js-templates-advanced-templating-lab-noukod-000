c9b2d0341548bd6d63777833781465baadc17e87
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-18 18:33:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  handlebarsSetup()
+  initForm()
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function initForm(){
+  let recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML
+  let template = Handlebars.compile(recipeFormTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+}
+
+function createRecipe(){
+  let recipe = getRecipeVals();
+  let recipeTemplate = document.getElementById("recipe-template").innerHTML;
+  let template = Handlebars.compile(recipeTemplate);
+  // document.getElementById("main").innerHTML = template(recipe)
+  let html = recipeTemplate(recipe)
+  
+}
+
+
+
